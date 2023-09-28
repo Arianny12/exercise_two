b56be962ec4af748489613c72db919a3d3712045
Arianny <ama10081@nyu.edu>
2023-09-28 09:27:28 -0400

    final files

--- src/pages/article/[slug].js
+++ src/pages/article/[slug].js
@@ -1,5 +1,7 @@
 import {useRouter} from "next/router";
+import "../../app/globals.css"
 import Data from '../../app/components/data';
+import styles from "./article.module.css"
 export default function Article(){
     const router = useRouter();
     const slug = router.query.slug;
@@ -8,10 +10,33 @@ export default function Article(){
     if(!articleData) return null;
     return (
         <main>
-            <h1>Article</h1>
-            <p>{articleData.id}</p>
-            <img src={articleData.image.url} alt={articleData.image.alt}/>
-            <p>{articleData.blurb}</p>
+            <div className ={styles.articleHeader} style={ {backgroundImage: `url('${articleData.image.url}')`}}>
+                <div className ={styles.articleWrapper}>
+                    <h1>{articleData.title}</h1>
+                    <p>{new Date(articleData.publishedDate).toDateString()}</p>
+                    <p className={styles.articleHeaderBlurb}>{articleData.blurb}</p>
+                </div>
+            </div>
+           
+            <div className ={styles.articleBody}>
+
+                <div className={styles.articleWrapper}>
+
+                    {articleData.articleText.map((textChunk)=> {
+                        switch(textChunk.type){
+                            case 'p':
+                                return <p>{textChunk.data}</p>
+                            case 'h2':
+                                return <h2>{textChunk.data}</h2>
+                            case "h3":
+                                return <h3>{textChunk.data}</h3>
+                            default:
+                                return <p>{textChunk.data}</p>
+                        }
+                    })}
+                
+                </div>
+            </div>
         </main>
     );
 };
